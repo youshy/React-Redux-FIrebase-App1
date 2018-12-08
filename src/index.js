2035a7a4d909a55e997f24b6801569bb1a781011
Artur <devilyoushy@gmail.com>
2018-12-08 16:31:28 +0000

    Added loader

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
+import Spinner from "./Spinner";
 import register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
 import firebase from "./firebase";
@@ -34,7 +35,9 @@ class Root extends React.Component {
   }
 
   render() {
-    return (
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
@@ -44,9 +47,13 @@ class Root extends React.Component {
   }
 }
 
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading
+});
+
 const RootWithAuth = withRouter(
   connect(
-    null,
+    mapStateToProps,
     { setUser }
   )(Root)
 );
