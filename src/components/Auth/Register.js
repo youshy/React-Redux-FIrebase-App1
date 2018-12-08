fc249176a48c1a8f0667faa729a9a58de50ad1d4
Artur <devilyoushy@gmail.com>
2018-12-08 13:49:22 +0000

    Created register form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -8,10 +8,71 @@ import {
   Message,
   Icon
 } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 class Register extends Component {
+  state = {};
+
+  handleChange = () => {};
+
   render() {
-    return <div>Register</div>;
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" textAlign="center">
+            <Icon name="puzzle piece" color="orange" />
+            Don't slack, register straight away!
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email"
+                onChange={this.handleChange}
+                type="email"
+              />
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Form.Input
+                fluid
+                name="passwordConfirmation"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Confirm password"
+                onChange={this.handleChange}
+                type="password"
+              />
+
+              <Button color="orange" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a user? <Link to="/login">Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
