da820742129878c23ad9ff0dca2c0682c1ac1883
Artur <devilyoushy@gmail.com>
2018-12-08 16:58:05 +0000

    User panel component

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
+import UserPanel from "./UserPanel";
 
 class SidePanel extends Component {
   render() {
-    return <div>SidePanel</div>;
+    return (
+      <Menu
+        size="large"
+        interted
+        fixed="left"
+        vertical
+        style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
+      >
+        <UserPanel />
+      </Menu>
+    );
   }
 }
 
