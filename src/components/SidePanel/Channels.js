32f43103c73bb1239ce6f2fd95fc497d33ada083
Artur <devilyoushy@gmail.com>
2018-12-08 20:52:15 +0000

    Switching channels via Redux state enabled

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import firebase from "../../firebase";
+import { connect } from "react-redux";
+import { setCurrentChannel } from "../../actions";
 import { Menu, Icon, Modal, Form, Button, Input } from "semantic-ui-react";
 
 class Channels extends Component {
@@ -59,19 +61,23 @@ class Channels extends Component {
     }
   };
 
+  isFormValid = ({ channelName, channelDetails }) =>
+    channelName && channelDetails;
+
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  isFormValid = ({ channelName, channelDetails }) =>
-    channelName && channelDetails;
+  changeChannel = channel => {
+    this.props.setCurrentChannel(channel);
+  };
 
   displayChannels = channels =>
     channels.length > 0 &&
     channels.map(channel => (
       <Menu.Item
         key={channel.id}
-        onClick={() => console.log(channel)}
+        onClick={() => this.changeChannel(channel)}
         name={channel.name}
         style={{ opacity: 0.7 }}
       >
@@ -136,4 +142,7 @@ class Channels extends Component {
   }
 }
 
-export default Channels;
+export default connect(
+  null,
+  { setCurrentChannel }
+)(Channels);
