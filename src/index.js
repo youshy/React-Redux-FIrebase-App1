c08b402ea32cdb34e488af8e7256ffed315153bb
Artur <devilyoushy@gmail.com>
2018-12-08 16:00:40 +0000

    Login to index and register to index route changes

--- src/index.js
+++ src/index.js
@@ -4,22 +4,43 @@ import App from "./components/App";
 import Login from "./components/Auth/Login";
 import register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
+import firebase from "./firebase";
 
 import "semantic-ui-css/semantic.min.css";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 
-const Root = () => {
-  return (
-    <Router>
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={register} />
       </Switch>
-    </Router>
-  );
-};
+    );
+  }
+}
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
