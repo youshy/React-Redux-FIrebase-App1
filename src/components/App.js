f4e777a0d3d3ca94f3ce725ab19aeb2a23a83830
Artur <devilyoushy@gmail.com>
2018-12-08 21:49:07 +0000

    Added messages on main screen

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,20 @@ import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 
-const App = ({ currentUser }) => {
+const App = ({ currentUser, currentChannel }) => {
   return (
     <Grid columns="equal" className="app" style={{ background: "#eee" }}>
       <ColorPanel />
-      <SidePanel currentUser={currentUser} />
+      <SidePanel
+        key={currentChannel && currentChannel.uid}
+        currentUser={currentUser}
+      />
       <Grid.Column style={{ marginLeft: 320 }}>
-        <Messages />
+        <Messages
+          key={currentChannel && currentChannel.id}
+          currentChannel={currentChannel}
+          currentUser={currentUser}
+        />
       </Grid.Column>
       <Grid.Column width={4}>
         <MetaPanel />
@@ -24,7 +31,8 @@ const App = ({ currentUser }) => {
 };
 
 const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel
 });
 
 export default connect(mapStateToProps)(App);
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -10,7 +10,7 @@ class SidePanel extends Component {
     return (
       <Menu
         size="large"
-        interted
+        inverted
         fixed="left"
         vertical
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
