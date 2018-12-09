ac109a31103ebe7f8669f83fd47d3234ecffecbd
Artur <devilyoushy@gmail.com>
2018-12-09 18:09:35 +0000

    meta panel basic functionality

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -15,7 +15,7 @@ const timeFromNow = timestamp => moment(timestamp).fromNow();
 export default function Message({ message, user }) {
   return (
     <Comment>
-      <Comment.Avatar src={message.user.Avatar} />
+      <Comment.Avatar src={message.user.avatar} />
       <Comment.Content className={isOwnMessage(message, user)}>
         <Comment.Author as="a">{message.user.name}</Comment.Author>
         <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
