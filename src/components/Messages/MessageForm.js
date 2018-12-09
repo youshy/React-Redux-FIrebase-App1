09ca51acbeac679ed573ef254014e23f202b437a
Artur <devilyoushy@gmail.com>
2018-12-09 13:33:12 +0000

    Pluralize on users on channel count

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -166,6 +166,7 @@ class MessageForm extends Component {
           />
           <Button
             color="teal"
+            disabled={uploadState === "uploading"}
             onClick={this.openModal}
             content="Upload media"
             labelPosition="right"
