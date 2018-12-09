61715efee93e9a32e1047e97b25043d3ff163aab
Artur <devilyoushy@gmail.com>
2018-12-09 11:50:31 +0000

    Added file modal

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import firebase from "../../firebase";
 import { Segment, Button, Input } from "semantic-ui-react";
 
+import FileModal from "./FileModal";
+
 class MessageForm extends Component {
   state = {
     message: "",
     channel: this.props.currentChannel,
     user: this.props.currentUser,
     loading: false,
-    errors: []
+    errors: [],
+    modal: false
   };
+
+  openModal = () => this.setState({ modal: true });
+
+  closeModal = () => this.setState({ modal: false });
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
@@ -55,7 +62,7 @@ class MessageForm extends Component {
   };
 
   render() {
-    const { errors, message, loading } = this.state;
+    const { errors, message, loading, modal } = this.state;
 
     return (
       <Segment className="message__form">
@@ -85,10 +92,12 @@ class MessageForm extends Component {
           />
           <Button
             color="teal"
+            onClick={this.openModal}
             content="Upload media"
             labelPosition="right"
             icon="cloud upload"
           />
+          <FileModal modal={modal} closeModal={this.closeModal} />
         </Button.Group>
       </Segment>
     );
